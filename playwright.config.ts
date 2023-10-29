4722358a100dc6ceea664b9d10e1716ecaae0cfe
Amanda Stecz <amandasstecz@gmail.com>
2023-10-29 02:09:48 -0300

    testes multi browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    baseURL: 'https://loginxp.vercel.app'
+    baseURL: 'https://loginxp.vercel.app',
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
@@ -38,15 +39,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
