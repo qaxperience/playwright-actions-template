01f0678737cc47e5bf8cdb110eb1b507895023b6
Amanda Stecz <amandasstecz@gmail.com>
2023-10-29 01:28:08 -0300

    fix: correção do cenário de login com usuário obrigatório e inclusão da url base

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'https://loginxp.vercel.app'
   },
 
   /* Configure projects for major browsers */
@@ -37,15 +38,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
